5d7e37fc66139fbee2b4cacb00c14afc3eb46200
chadcollins <chadrcollins91@gmail.com>
2018-05-19 17:49:49 -0400

    Suppressing error

--- src/main/kotlin/index.kt
+++ src/main/kotlin/index.kt
@@ -1,10 +1,15 @@
 import react.dom.*
 import kotlin.browser.document
 
+external var Kotlin: dynamic
+
 fun main(args: Array<String>) {
     render(document.getElementById("content")) {
         h1 {
             +"Hello Kotlin React"
         }
     }
+
+    // Suppressing defineModule not a function error until it can be fixed
+    Kotlin.defineModule = {module: String -> module}
 }
