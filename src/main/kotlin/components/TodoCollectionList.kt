a8a12ad6cffa02d42b7c31bcd14d3f4680604941
chadcollins <chadrcollins91@gmail.com>
2018-05-20 10:57:01 -0400

    Simplified components

--- src/main/kotlin/components/TodoCollectionList.kt
+++ src/main/kotlin/components/TodoCollectionList.kt
@@ -1,54 +1,40 @@
 package components
 
 import TodoCollection
-import kotlinx.css.*
+import kotlinx.css.ListStyleType
+import kotlinx.css.padding
+import kotlinx.css.px
 import kotlinx.html.js.onClickFunction
 import react.RBuilder
-import react.RComponent
-import react.RProps
-import react.RState
 import react.dom.aside
 import react.dom.h3
 import react.dom.li
-import react.dom.ul
 import styled.css
 import styled.styledUl
 
-class TodoCollectionList : RComponent<TodoCollectionListProps, RState>() {
-    override fun RBuilder.render() {
-        aside {
-            h3 {
-                +"Collections"
+fun RBuilder.todoCollectionList(
+        collectionList: ArrayList<TodoCollection>,
+        onListSelected: (index: Int) -> Unit) {
+    aside {
+        h3 {
+            +"Collections"
+        }
+
+        styledUl {
+            css {
+                listStyleType = ListStyleType.none
+                padding(0.px)
             }
 
-            styledUl {
-                css {
-                    listStyleType = ListStyleType.none
-                    padding(0.px)
-                }
+            collectionList.mapIndexed { index: Int, collection: TodoCollection ->
+                li {
+                    +collection.name
 
-                props.collectionList.mapIndexed { index: Int, collection: TodoCollection ->
-                    li {
-                        +collection.name
-
-                        attrs {
-                            onClickFunction = { props.onListSelected(index) }
-                        }
+                    attrs {
+                        onClickFunction = { onListSelected(index) }
                     }
                 }
             }
         }
     }
-}
-
-class TodoCollectionListProps(
-        var collectionList: ArrayList<TodoCollection>,
-        var onListSelected: (index: Int) -> Unit
-) : RProps
-
-fun RBuilder.todoCollectionList(
-        collectionList: ArrayList<TodoCollection>,
-        onListSelected: (index: Int) -> Unit) = child(TodoCollectionList::class) {
-    attrs.collectionList = collectionList
-    attrs.onListSelected = onListSelected
 }
--- src/main/kotlin/components/TodoItem.kt
+++ src/main/kotlin/components/TodoItem.kt
@@ -3,40 +3,22 @@ package components
 import kotlinx.html.InputType
 import kotlinx.html.js.onClickFunction
 import react.RBuilder
-import react.RComponent
-import react.RProps
-import react.RState
 import react.dom.input
 import react.dom.label
 import react.dom.li
 
-class TodoItem: RComponent<TodoItemProps, RState>() {
+fun RBuilder.todoItem(text: String, completed: Boolean, onClick: () -> Unit) {
+    li {
+        label {
+            +text
 
-    override fun RBuilder.render() {
-        li {
-            label {
-                +props.text
-
-                input {
-                    attrs {
-                        type = InputType.checkBox
-                        checked = props.completed
-                        onClickFunction = { props.onClick()}
-                    }
+            input {
+                attrs {
+                    type = InputType.checkBox
+                    checked = completed
+                    onClickFunction = { onClick() }
                 }
             }
         }
     }
-}
-
-class TodoItemProps(
-        var text: String,
-        var completed: Boolean,
-        var onClick: () -> Unit
-) : RProps
-
-fun RBuilder.todoItem(text: String, completed: Boolean, onClick: () -> Unit) = child(TodoItem::class) {
-    attrs.text = text
-    attrs.completed = completed
-    attrs.onClick = onClick
 }
