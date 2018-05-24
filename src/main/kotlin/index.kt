2acd443a456f809e4d8ea507977ad246092b3dcf
Chad Collins <ccollins@dailybreak.com>
2018-05-24 08:28:50 -0400

    Removed unused code

--- src/main/kotlin/index.kt
+++ src/main/kotlin/index.kt
@@ -1,21 +1,11 @@
 import components.styles.TodoStyles
 import components.todoApp
 import kotlinext.js.invoke
-import kotlinx.css.CSSBuilder
-import kotlinx.css.body
-import kotlinx.css.px
 import react.dom.render
 import styled.StyledComponents
 import kotlin.browser.document
 
 external var Kotlin: dynamic
-
-val styles = CSSBuilder().apply {
-    body {
-        fontSize = 16.px
-        fontFamily = "Arial"
-    }
-}
 
 fun main(args: Array<String>) {
     render(document.getElementById("content")) {
