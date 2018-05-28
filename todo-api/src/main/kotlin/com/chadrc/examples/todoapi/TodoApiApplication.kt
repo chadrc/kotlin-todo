07a44b4cdd5683f4d1151ac9af12978ce22d3178
chadcollins <chadrcollins91@gmail.com>
2018-05-27 20:30:23 -0400

    Added todo api sub project

---
+++ todo-api/src/main/kotlin/com/chadrc/examples/todoapi/TodoApiApplication.kt
@@ -0,0 +1,11 @@
+package com.chadrc.examples.todoapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<TodoApiApplication>(*args)
+}
---
+++ todo-api/src/test/kotlin/com/chadrc/examples/todoapi/TodoApiApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.chadrc.examples.todoapi
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TodoApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
