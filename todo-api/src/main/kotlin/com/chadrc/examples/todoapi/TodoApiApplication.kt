f53857cdef56e64e4bd57b996ab3ddd0f18a4f9b
chadcollins <chadrcollins91@gmail.com>
2018-05-28 20:49:30 -0400

    Moved main method to its own file

--- todo-api/src/main/kotlin/com/chadrc/examples/todoapi/TodoApiApplication.kt
+++ todo-api/src/main/kotlin/com/chadrc/examples/todoapi/TodoApiApplication.kt
@@ -1,11 +1,7 @@
 package com.chadrc.examples.todoapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class TodoApiApplication
 
-fun main(args: Array<String>) {
-    runApplication<TodoApiApplication>(*args)
-}
