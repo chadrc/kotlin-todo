fa8779413deecb63c1de64a73304eae7312cb59b
chadcollins <chadrcollins91@gmail.com>
2018-05-28 20:01:50 -0400

    Implemented delete collection and delete todo endpoints

--- todo-api/src/main/kotlin/com/chadrc/examples/todoapi/TodoCollectionController.kt
+++ todo-api/src/main/kotlin/com/chadrc/examples/todoapi/TodoCollectionController.kt
@@ -12,6 +12,7 @@ class TodoCollectionController(val todoCollectionRepository: TodoCollectionRepos
     fun findAll(): Iterable<TodoCollection> = todoCollectionRepository.findAll()
 
     data class CreateRequest(val name: String)
+
     @PostMapping
     fun create(@RequestBody request: CreateRequest) =
             todoCollectionRepository.save(TodoCollection(request.name))
@@ -27,6 +28,28 @@ class TodoCollectionController(val todoCollectionRepository: TodoCollectionRepos
             collection.todos.add(todo)
             todoCollectionRepository.save(collection)
             return ResponseEntity.ok(todo)
+        }
+
+        return ResponseEntity.notFound().build()
+    }
+
+    @DeleteMapping
+    fun deleteCollection(@RequestParam collectionId: String) = todoCollectionRepository.deleteById(ObjectId(collectionId))
+
+    @DeleteMapping("/todo")
+    fun deleteTodo(@RequestParam collectionId: String, @RequestParam todoIndex: Int): ResponseEntity<Any> {
+        if (todoIndex < 0) {
+            return ResponseEntity.badRequest().body("todoIndex must be a positive integer.")
+        }
+
+        val optional = todoCollectionRepository.findById(ObjectId(collectionId))
+        if (optional.isPresent) {
+            val collection = optional.get()
+            if (todoIndex >= collection.todos.size) {
+                return ResponseEntity.notFound().build()
+            }
+            collection.todos.removeAt(todoIndex)
+            todoCollectionRepository.save(collection)
         }
 
         return ResponseEntity.notFound().build()
